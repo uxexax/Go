c11f3eedc71b2d908caad0d2cae25c45ef37e338
uxexax <ux.ex.ax@gmail.com>
2018-12-01 11:47:05 +0100

    Short descriptions added.

--- FunMethInt/Animals/animals.go
+++ FunMethInt/Animals/animals.go
@@ -1,3 +1,11 @@
+/* -----
+This program is a class/object exercise. It maintains pieces of information about certain
+animals, which can be queried via the standard input commands. The input is looped until
+the user gives the 'leave' command. An animal information request has to be in the
+'<animal> <information>' format. Valid animal values: cow, bird, snake. Valid information
+values: eat, move, speak.
+----- */
+
 package main
 
 import (
